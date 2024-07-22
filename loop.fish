8e1d6cfaac695e56507ade9b9e736e68fc35d067
david watson <davidthewatson@gmail.com>
2024-07-22 14:44:12 -0400

    Fix formatting

--- loop.fish
+++ loop.fish
@@ -3,13 +3,13 @@
 echo 'Waiting on the world to change...'
 
 inotifywait -m ./markdown/*.md -e modify |
-while read -r directory file action
-    if string match -r '.*\.md$' "$file"
-        make all
-        cp ./output/cv.html ../davidthewatson.github.io/docs/cv/.
-        cp ./output/cv.pdf ../davidthewatson.github.io/docs/cv/.
-        cp ./output/cv.docx ../davidthewatson.github.io/docs/cv/.
-        cp ./output/cv.rtf ../davidthewatson.github.io/docs/cv/.
-        cp ./output/cv.txt ../davidthewatson.github.io/docs/cv/.
+    while read -r directory file action
+        if string match -r '.*\.md$' "$file"
+            make all
+            cp ./output/*.html ../davidthewatson.github.io/docs/cv/.
+            cp ./output/*.pdf ../davidthewatson.github.io/docs/cv/.
+            cp ./output/*.docx ../davidthewatson.github.io/docs/cv/.
+            cp ./output/*.rtf ../davidthewatson.github.io/docs/cv/.
+            cp ./output/*.txt ../davidthewatson.github.io/docs/cv/.
+        end
     end
-end
